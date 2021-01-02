f06abc44b9e8a6914effbf355870071405f5881f
robin-castellani <robin.castellani@alumni.unitn.it>
2021-01-02 22:56:46 +0100

    Feat: function to read the substitution file

--- src/medicus_politicus/main.py
+++ src/medicus_politicus/main.py
@@ -14,6 +14,7 @@ The idea is to run a CLI command like
 
 
 import pathlib
+from typing import List, Tuple
 
 import pdfminer.high_level
 
@@ -33,6 +34,37 @@ def read_pdf(filepath: pathlib.Path) -> str:
     """Read a .pdf file"""
     text = pdfminer.high_level.extract_text(filepath)
     return text
+
+
+@memoizer.memoize()
+def read_substitutions(filepath: pathlib.Path) -> List[Tuple[str, str]]:
+    """
+    Read the file with the substitutions to perform.
+
+    The file format is the following:
+
+    ::
+
+        original_string1|new_string1
+        original_string2|new_string2
+        ...
+
+
+    :param filepath: path to the file to read.
+    :return: List with the tuple (original_string,new_string)
+    """
+    # TODO: check for the file existence and that the file is a file
+
+    # read the file as a text
+    substitutions_str = filepath.read_text(encoding='utf-8')
+    substitutions_list = substitutions_str.split('\n')
+    substitutions = [
+        tuple(sub.split('|'))
+        for sub in substitutions_list
+    ]
+
+    return substitutions
+
 
 """
 @click.command()
