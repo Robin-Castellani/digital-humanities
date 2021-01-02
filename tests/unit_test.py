5bed74a61b3b97653f454b41e057d5fdef7cfd9b
robin-castellani <robin.castellani@alumni.unitn.it>
2021-01-02 22:57:09 +0100

    Test: function to read the substitution file

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -13,7 +13,23 @@ def pdf_test() -> pathlib.Path:
     return pathlib.Path('./file_fixtures/test.pdf')
 
 
+@pytest.fixture
+def substitutions_test() -> pathlib.Path:
+    """Path to the test substitutions .csv file."""
+    return pathlib.Path('./file_fixtures/substitutions.csv')
+
+
 def test_read_pdf(pdf_test):
     text = main.read_pdf(pdf_test)
 
     assert text.strip() == 'Test test'
+
+
+def test_read_substitutions(substitutions_test):
+    substitutions = main.read_substitutions(substitutions_test)
+    expected_substitutions = [
+        ("old_string", "new_string"),
+        ("very_old_string", "super_new_string")
+    ]
+
+    assert substitutions == expected_substitutions
