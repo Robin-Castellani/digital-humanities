764b1547c1a319186280807d998ecc079e608d45
robin-castellani <robin.castellani@alumni.unitn.it>
2021-01-03 18:13:45 +0100

    Feat: CLI to substitute text from a .pdf file

--- src/medicus_politicus/main.py
+++ src/medicus_politicus/main.py
@@ -16,6 +16,8 @@ The idea is to run a CLI command like
 import pathlib
 from typing import List, Tuple
 
+import click
+import click_pathlib
 import pdfminer.high_level
 
 from . import helpers
@@ -24,6 +26,11 @@ from . import helpers
 memoizer = helpers.Memoizer(cache_dir='./cache')
 
 
+@click.group(
+    help="Insieme di tool per digitalizzare la nostra umanità! "
+         "Usa uno dei comandi qui sotto... "
+         "Nel dubbio, scrivi il nome del comando seguito da --help"
+)
 def cli():
     """Implement the Command Line Interface."""
     pass
@@ -83,16 +90,57 @@ def perform_substitutions(
     return text
 
 
-"""
-@click.command()
-@click.option("--file", help="file originale")
-@click.option("--file-sostituzioni", help="File con le sostituzioni da effettuare")
-@click.option("--output-name", help="Nome del file risultante.")
-def sostituisci(file, file_sostituzioni, output_name):
-    f = leggi_file(file)
-    f_sostituito = effettua_sostituzioni(f, file_sostituzioni)
-    scrivi_file(f_sostituito, name=output_name)
-"""
+@cli.command(
+    help="Legge il testo di un file .pdf; "
+         "legge un file .csv contenente le sostituzioni da effettuare; "
+         "effettua le sostituzioni e salva il testo risultante in un .txt; "
+         "il file viene salvato nella stessa cartella del file .pdf.\n"
+         "ATTENZIONE: se è presente un file .txt con lo stesso nome, "
+         "questo viene sovrascritto."
+)
+@click.option(
+    "--file-pdf",
+    required=True,
+    type=click_pathlib.Path(exists=True),
+    help="Percorso del file .pdf originale"
+)
+@click.option(
+    "--file-sostituzioni",
+    required=True,
+    type=click_pathlib.Path(exists=True),
+    help="File .csv con le sostituzioni da effettuare; "
+         "il formato deve essere di questo tipo:\n"
+         "stringa_vecchia|stringa_nuova"
+)
+@click.option(
+    "--nome-output",
+    default='result',
+    show_default=True,
+    help="Nome del file .txt risultante"
+)
+def sostituisci(
+        file_pdf: pathlib.Path, file_sostituzioni: pathlib.Path,
+        nome_output: str
+) -> None:
+    """
+    Read a .pdf file and a substitutions .csv file;
+    perform the substitutions and then save the text in a .txt file.
+    
+    :param file_pdf: name of the .pdf input file.
+    :param file_sostituzioni: name of the .csv input file with substitutions.
+    :param nome_output: name of the .txt output file.
+    :return: None.
+    """
+    # read the text in the .pdf file
+    pdf = read_pdf(file_pdf)
+    # read the .txt substitutions file
+    substitutions = read_substitutions(file_sostituzioni)
+    # perform the substitutions
+    f_substituted = perform_substitutions(pdf, substitutions)
+    # save the file
+    output_filepath = file_pdf.with_name(nome_output + '.txt')
+    output_filepath.write_text(data=f_substituted, encoding='utf-8')
+
 
 if __name__ == '__main__':
     cli()
